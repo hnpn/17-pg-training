dac11ec1975843acdc2c2941c288b6e8272a2361
hnpn <hnpn@pixiv.com>
2017-05-01 15:38:48 +0900

    トゥート情報を保存できるようになりました

--- functions.php
+++ functions.php
@@ -21,3 +21,23 @@ function redirectToLoginPageIfNotLoggedIn() {
         exit;
     }
 }
+
+/**
+ * ファイルをサーバーに保存して、保存したファイル名を返す
+ *
+ * @param $tmp_file_path $_FILES['uploaded_file']
+ * @return string 保存したファイル名
+ */
+function uploadFile($file){
+  // 良い感じにかぶらないファイル名を作る
+  $image_file_name = md5(mt_rand()) . '.' . pathinfo($file['name'], PATHINFO_EXTENSION);
+  $upload_path = dirname(__FILE__) . '/uploaded_image/' . $image_file_name;
+
+  // その場所に保存してみてダメだったらエラー
+  if (!move_uploaded_file($file['tmp_name'], $upload_path)) {
+    throw new Exception('なんかやばいエラー', 1);
+  }
+
+  // 保存したファイル名返す
+  return $image_file_name;
+}
--- post_toot.php
+++ post_toot.php
@@ -5,4 +5,30 @@ require_once 'functions.php';
 session_start();
 redirectToLoginPageIfNotLoggedIn();
 
+// 保存処理を書く
+
+// var_dump($_POST);
+$text = $_POST['text'];
+$user_id = $_SESSION['user_id'];
+$created_at = date("Y-m-d H:i:s");
+$image_file_name = uploadFile($_FILES["image"]);
+
+//ファイルアップロード
+
+$database = getDatabase();
+// var_dump($database); exit();
+$database->query("
+    INSERT INTO `toot` (
+      `user_id`,
+      `text`,
+      `image_file_name`,
+      `created_at`
+    ) VALUES (
+      '{$user_id}',
+      '{$text}',
+      '{$image_file_name}',
+      '{$created_at}'
+    )
+");
+
 header('Location: /');
